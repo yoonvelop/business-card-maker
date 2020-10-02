0ac44e603d512aafa8ef2089ea972891a498dbf2
yoonvelop <yoonvelop@gmail.com>
2020-10-02 16:21:25 +0900

    routing , logout

---
+++ src/components/maker/Maker.js
@@ -0,0 +1,28 @@
+import React, { useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import Footer from "../footer/Footer";
+import Header from "../header/Header";
+import styles from "./Maker.module.css";
+
+const Maker = ({ authService }) => {
+  const history = useHistory();
+  const onLogout = () => {
+    authService.logout();
+  };
+
+  useEffect(() => {
+    authService.onAuthChange((user) => {
+      if (!user) {
+        history.push("/");
+      }
+    });
+  });
+  return (
+    <section className={styles.maker}>
+      <Header onLogout={onLogout} />
+      <Footer />
+    </section>
+  );
+};
+
+export default Maker;
